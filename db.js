4d3cb779779d9b30fa1500fad00a5307aeefd435
2t003 <114591938+2t003@users.noreply.github.com>
2024-12-04 16:12:16 +0100

    db et fichier db.js

---
+++ db.js
@@ -0,0 +1,11 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('Vinyl.db');
+
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS Product (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, prix REAL, stock INTEGER, genre TEXT, type TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS Account (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, prenom TEXT, mail TEXT)");
+    db.run("CREATE TABLE IF NOT EXISTS Orders (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, prix_total REAL, account TEXT)");
+});
+
+db.close();
